f3eaf953614013071c7639b34dcf404c936aeb84
Avinash Sahani <avinashsahani66@gmail.com>
2024-03-07 03:11:01 -0500

    Added Quick Sort Algo

---
+++ Striver-DSA-A-Z/01-Sorting/Sorting-II/QuickSort.cs
@@ -0,0 +1,43 @@
+namespace Striver_DSA_A_Z._01_Sorting;
+
+public partial  class  SortingAlgo
+{
+    public static void quickSort(int[] input, int startIndex, int endIndex)
+    {
+        if (startIndex < endIndex)
+        {
+
+            int partitionIndex = GetPartitionIndex(input, startIndex, endIndex);
+            quickSort(input,startIndex,partitionIndex-1);
+            quickSort(input,partitionIndex+1,endIndex);
+
+        }
+    }
+
+    public static int GetPartitionIndex(int[] arr, int startIndex, int endIndex)
+    {
+
+        int low = startIndex;
+        int high = endIndex;
+        int pivot = arr[low];
+        int i = low+1;
+        int j = high;
+        while (i<=j)
+        {
+            while (  i <=high && arr[i] <= pivot )
+            {
+                i++;
+            }
+        
+            while (j>=low+1 && arr[j] >= pivot )
+            {
+                j--;
+            }
+            if(i<j)
+             (arr[i], arr[j]) = (arr[j], arr[i]);
+        }
+        (arr[low], arr[j]) = (arr[j], arr[low]);
+        return j;
+      
+    }
+}
--- Striver-DSA-A-Z/DriverCode.cs
+++ Striver-DSA-A-Z/DriverCode.cs
@@ -5,7 +5,7 @@
 using Striver_DSA_A_Z._01_Sorting;
 
 int[] arr = { 64, 25, 12, 22, 11 };
-SortingAlgo.MergeSort(arr,0,arr.Length-1);
+SortingAlgo.quickSort(arr,0,arr.Length-1);
 Console.WriteLine("Sorted array:");
 foreach (int num in arr)
 {
