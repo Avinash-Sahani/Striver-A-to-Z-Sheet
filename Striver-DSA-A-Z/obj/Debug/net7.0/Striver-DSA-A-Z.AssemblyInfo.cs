da8c60dc756fc855217d2d152aeeb568fb808829
Avinash Sahani <avinashsahani66@gmail.com>
2024-03-12 23:08:50 -0400

    Updated gitgnore

--- Striver-DSA-A-Z/obj/Debug/net7.0/Striver-DSA-A-Z.AssemblyInfo.cs
+++ Striver-DSA-A-Z/obj/Debug/net7.0/Striver-DSA-A-Z.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Striver-DSA-A-Z")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9b9ebf8efd6db2dee03ca31c33302496fd857512")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c1c05b7e139a26663909b732f63dcd47169ac586")]
 [assembly: System.Reflection.AssemblyProductAttribute("Striver-DSA-A-Z")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Striver-DSA-A-Z")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
