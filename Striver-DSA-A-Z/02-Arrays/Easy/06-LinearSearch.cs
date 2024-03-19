aa9d5d47fd287e95fdf6bcdf4b434e03feadd13d
Avinash Sahani <avinashsahani66@gmail.com>
2024-03-19 00:22:09 -0400

    Added linear search problem code

---
+++ Striver-DSA-A-Z/02-Arrays/Easy/06-LinearSearch.cs
@@ -0,0 +1,17 @@
+namespace Striver_DSA_A_Z._02_Arrays.Easy;
+
+public partial class Arrays {
+    public static int linearSearch(int n, int num, int []arr){
+        int index = -1;
+        for(int i=0; i<n; i++)
+        {
+            if(arr[i] == num)
+            {
+                index = i;
+                break;
+            }
+        }
+        return index;
+        // Write your code here.
+    }
+}
