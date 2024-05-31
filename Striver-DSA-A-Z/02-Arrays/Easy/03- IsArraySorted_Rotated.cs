6476fad3367c9da101fa2a4470ecaf602a73caf8
Avinash Sahani <avinashsahani66@gmail.com>
2024-05-31 02:52:13 -0400

    update array sorted rotated code

---
+++ Striver-DSA-A-Z/02-Arrays/Easy/03- IsArraySorted_Rotated.cs
@@ -0,0 +1,22 @@
+namespace Striver_DSA_A_Z._02_Arrays.Easy;
+
+public partial class Arrays{
+    public  static bool IsRotatedSorted(int[] nums)
+    {
+        if (nums.Length == 1)
+            return true;
+        var isSorted = true;
+        int count = 0;
+        int currentIndex = 1;
+        for (int i = 1; i < nums.Length; i++)
+        {
+            if(nums[i]<nums[i-1])
+                count++;
+         
+        }
+        if( nums[nums.Length -1 ] > nums[0])
+            count++;
+        return count<=1;
+    }
+    
+}
