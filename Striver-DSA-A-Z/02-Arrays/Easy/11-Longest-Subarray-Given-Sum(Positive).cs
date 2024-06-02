af00bac7ffd30353d167b64b3a3295c010871e93
Avinash Sahani <avinashsahani66@gmail.com>
2024-06-02 03:02:20 -0400

    Arrays-> change the naming of the problem

---
+++ Striver-DSA-A-Z/02-Arrays/Easy/11-Longest-Subarray-Given-Sum(Positive).cs
@@ -0,0 +1,56 @@
+namespace Striver_DSA_A_Z._02_Arrays.Easy;
+
+public partial class Arrays
+{
+    public static int longestSubarrayWithSumK(int []a, long k) {
+
+    
+
+        int left = 0;
+        int right = 1;
+        long sum = a[0];
+        int n = a.Length;
+        int maxLength = -1;
+        while(right<n)
+        {
+
+
+            while(sum >k && left<right )
+            { 
+                sum -= a[left];
+                left++;
+            }
+
+        
+            
+            if(sum==k)
+            {
+
+                int length = (right - left) ; 
+                maxLength = Math.Max(length, maxLength);
+                    
+            }
+            
+            if(right<n)
+                sum+= a[right];
+            
+            right++;
+
+
+
+
+
+        }
+
+
+
+
+
+        return maxLength;
+
+       
+
+
+      
+    }
+}
