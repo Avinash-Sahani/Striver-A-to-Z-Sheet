22d1bd0eb09d598bd8bfab93bdb5df1c5728495e
Avinash Sahani <avinashsahani66@gmail.com>
2024-06-04 02:20:44 -0400

    Arrays(Medium) -> SortColors

---
+++ Striver-DSA-A-Z/02-Arrays/Medium/02-SortColors-.cs
@@ -0,0 +1,36 @@
+namespace Striver_DSA_A_Z._02_Arrays.Medium;
+//Link : https://leetcode.com/problems/sort-colors/
+public partial class Arrays {
+    public void SortColors(int[] nums) {
+
+        int n = nums.Length;
+        int low =0,high =n-1 ,mid =0;   
+    
+    
+        while(mid<=high)
+        {
+
+            if(nums[mid] ==0)
+            {
+
+                (nums[low],nums[mid]) = (nums[mid],nums[low]);
+                low++;
+                mid++;
+            }
+            else if(nums[mid] ==1)
+                mid++;
+
+            else if(nums[mid]==2)
+            {
+            
+                (nums[mid],nums[high]) = (nums[high],nums[mid]);
+                high--;
+            }
+
+
+
+
+        }
+    
+    }
+}
