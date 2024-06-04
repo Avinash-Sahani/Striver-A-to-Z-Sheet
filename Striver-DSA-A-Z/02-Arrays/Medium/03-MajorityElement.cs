71f145bc9892669ad5f70f060af88e904ff527e3
Avinash Sahani <avinashsahani66@gmail.com>
2024-06-04 02:24:16 -0400

    Arrays -> MajoirtyElement

---
+++ Striver-DSA-A-Z/02-Arrays/Medium/03-MajorityElement.cs
@@ -0,0 +1,27 @@
+namespace Striver_DSA_A_Z._02_Arrays.Medium;
+
+public partial class Arrays
+{
+    public int MajorityElement(int[] nums)
+    {
+        int count = 0;
+        int element = -1;
+        int n = nums.Length;
+        for (int i = 0; i < n; i++)
+        {
+            if (count == 0)
+            {
+                element = nums[i];
+            }
+
+            if (nums[i] == element)
+            {
+                count++;
+            }
+            else
+                count--;
+        }
+
+        return element;
+    }
+}
