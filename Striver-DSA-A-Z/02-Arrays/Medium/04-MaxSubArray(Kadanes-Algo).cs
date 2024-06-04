79772493e785e993cea25e4ee78cdf0cbd9c135d
Avinash Sahani <avinashsahani66@gmail.com>
2024-06-04 02:43:57 -0400

    Arrays(Medium) -> Max Subarray

---
+++ Striver-DSA-A-Z/02-Arrays/Medium/04-MaxSubArray(Kadanes-Algo).cs
@@ -0,0 +1,21 @@
+namespace Striver_DSA_A_Z._02_Arrays.Medium;
+
+public partial class Arrays_
+{
+    public int MaxSubArray(int[] nums)
+    {
+        int max_sum = int.MinValue;
+
+        int sum = 0;
+        for (int i = 0; i < nums.Length; i++)
+        {
+            if (sum < 0)
+                sum = 0;
+            sum += nums[i];
+            if (sum > max_sum)
+                max_sum = sum;
+        }
+
+        return max_sum;
+    }
+}
