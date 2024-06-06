6e46ba71b4db2e01892472a23ad543ad6ab4ef20
Avinash Sahani <avinashsahani66@gmail.com>
2024-06-06 03:37:14 -0400

    Arrays(Medium) -> Longest consecutive in array

---
+++ Striver-DSA-A-Z/02-Arrays/Medium/09-LongestConsecutiveInArray.cs
@@ -0,0 +1,32 @@
+namespace Striver_DSA_A_Z._02_Arrays.Medium;
+
+public partial class Arrays {
+    public int LongestConsecutive(int[] nums)
+    {
+        int max = 0;
+        int count = 0;
+        HashSet<int> numsSet = new HashSet<int>();
+        foreach (var num in nums)
+        {
+            numsSet.Add(num);
+        }
+        
+        foreach (var num in nums)
+        {
+            count = 0;
+            if (!numsSet.Contains(num - 1))
+            {
+                int small = num;
+                
+                while (numsSet.Contains(small++))
+                    count++;
+
+                max = Math.Max(count, max);
+            }
+            
+            
+        }
+
+        return max;
+    }
+}
