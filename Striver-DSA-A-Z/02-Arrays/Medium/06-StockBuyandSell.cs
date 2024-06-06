f7d3884a31c176b9532467654d6ac90b6486675d
Avinash Sahani <avinashsahani66@gmail.com>
2024-06-05 22:08:16 -0400

    Arrays(Medium) -> Added 2 problems stock and rearrange array

---
+++ Striver-DSA-A-Z/02-Arrays/Medium/06-StockBuyandSell.cs
@@ -0,0 +1,38 @@
+namespace Striver_DSA_A_Z._02_Arrays.Medium;
+
+public partial class Arrays{
+    public int MaxProfit(int[] prices) {
+        if(prices.Length ==1)
+            return 0;
+        int buy = prices[0];
+        int sell = -1;
+        int p = sell - buy;;
+        int max_profit = p;
+        for(int i=1; i<prices.Length;i++)
+        {   
+
+            if(prices[i] < buy)
+            {
+                buy = prices[i];
+                sell = -1;
+            }
+
+            else if( prices[i] > sell  )
+                sell = prices[i];
+
+            p = sell - buy;
+            if(p>max_profit )
+                max_profit = p;
+
+
+        }
+
+        if(max_profit < 0)
+            return 0;
+        return max_profit;
+
+
+        
+
+    }
+}
---
+++ Striver-DSA-A-Z/02-Arrays/Medium/07-RearrangeArray.cs
@@ -0,0 +1,26 @@
+namespace Striver_DSA_A_Z._02_Arrays.Medium;
+
+public partial class Arrays {
+    public int[] RearrangeArray(int[] nums) {
+        int p1=0;
+        int p2 =1;
+        int n = nums.Length;
+        int[] rearrangedarray = new int[n];
+      
+
+        for(int i=0;i<n;i++)
+        {
+            if(nums[i]>=0)
+            {
+                rearrangedarray[p1] = nums[i];
+                p1+=2;
+            }
+            else
+            {
+                rearrangedarray[p2] = nums[i];
+                p2+=2;
+            }
+        }
+        return rearrangedarray;
+    }
+}
