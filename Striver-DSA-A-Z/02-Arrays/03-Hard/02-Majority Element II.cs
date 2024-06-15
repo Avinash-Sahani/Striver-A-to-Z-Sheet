f16c7c9288cbc6e632a490464ea66febd4dfdd8c
Avinash Sahani <avinashsahani66@gmail.com>
2024-06-15 16:33:44 -0400

    Arrays => n/3 majority

---
+++ Striver-DSA-A-Z/02-Arrays/03-Hard/02-Majority Element II.cs
@@ -0,0 +1,61 @@
+﻿namespace Striver_DSA_A_Z._02_Arrays._03_Hard;
+
+public partial class Arrays {
+    public IList<int> MajorityElement(int[] nums) {
+
+        int count_1=0,count_2 = 0;
+        IList<int> majority = new List<int>();
+        int num1=int.MinValue,num2 = int.MinValue;
+        int n = nums.Length;
+        for(int i=0; i<n; i++)
+        {
+
+            if(count_1==0 && nums[i]!=num2)
+                num1 = nums[i];
+
+            if(count_2==0 && nums[i]!=num1)
+                num2 = nums[i];
+
+
+            if(nums[i]==num1)
+                count_1++;
+
+
+            else if(nums[i]==num2)
+                count_2++;
+
+            else{
+                count_1--;
+                count_2--;
+            }
+
+
+
+        }
+
+        decimal divVal = n/3;
+        decimal n_3 = Math.Floor(divVal);
+        n_3 = (int) n_3;
+        n_3++;
+        count_1=0;
+        count_2=0;
+        for(int i=0; i<n; i++)
+        {
+            if(nums[i] == num1)
+                count_1++;
+
+            if(nums[i] == num2)
+                count_2++;
+
+        }
+        if(count_1>=n_3)
+            majority.Add(num1);
+        if(count_2>=n_3)
+            majority.Add(num2);
+
+
+
+        return majority;
+
+
+    }}
