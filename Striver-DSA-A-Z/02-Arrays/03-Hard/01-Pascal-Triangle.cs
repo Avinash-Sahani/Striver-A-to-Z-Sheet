4012ad054f0fa3eba6c4347ebe1f0d7cbbf89399
Avinash Sahani <avinashsahani66@gmail.com>
2024-06-15 13:42:18 -0400

    Arrays (Hard) -> Pascal Triangle

---
+++ Striver-DSA-A-Z/02-Arrays/03-Hard/01-Pascal-Triangle.cs
@@ -0,0 +1,36 @@
+﻿namespace Striver_DSA_A_Z._02_Arrays._03_Hard;
+
+public partial class Arrays {
+    public int nCr(int n,int r)
+    {
+
+        long result= 1;
+
+        if(r>n-r)
+            r=n-r;
+
+        for(int i=0; i<r; i++)
+        {
+
+            result = result * (n-i);
+            result = result/(i+1);
+        }
+        return (int) result;
+    }
+    public IList<IList<int>> Generate(int numRows) {
+
+        IList<IList<int>> triangle = new List<IList<int>>();
+
+        for(int i=0; i<numRows; i++)
+        {
+            IList<int> temp = new List<int>();
+            for(int j=0; j<=i; j++)
+            {
+                int num = nCr(i,j);
+                temp.Add(num);
+            }
+            triangle.Add(temp);
+        }
+        return triangle;
+    }
+}
