b8cae700089740986c88790c55df7cbc2266b61b
Avinash Sahani <avinashsahani66@gmail.com>
2024-06-21 02:19:58 -0400

    Arrays -> add permuatation and leaders

---
+++ Striver-DSA-A-Z/02-Arrays/02-Medium/09-LeadersInAray.cs
@@ -0,0 +1,28 @@
+﻿namespace Striver_DSA_A_Z._02_Arrays;
+
+public partial class Arrays {
+
+    public List<int> Leaders(int n, int[] arr) {
+
+
+        List<int> leaders = new List<int>();
+
+        int max = arr[n-1];
+        leaders.Add(max);
+        for(int i=n-2; i>=0; i--)
+
+        {
+
+            if(arr[i]>=max)
+            {
+                leaders.Add(arr[i]);
+                max = arr[i];
+            }
+
+        }
+        leaders.Reverse();
+        // code here
+
+        return leaders;
+    }
+}
