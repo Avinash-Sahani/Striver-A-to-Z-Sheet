ec1334f4094337ff70a106443e778c09dc446f4d
Avinash Sahani <avinashsahani66@gmail.com>
2024-06-22 13:24:20 -0400

    Arrays(Revision) -> Optimze Rotate Array

--- Striver-DSA-A-Z/02-Arrays/01-Easy/04-RotateArray.cs
+++ Striver-DSA-A-Z/02-Arrays/01-Easy/04-RotateArray.cs
@@ -3,21 +3,15 @@ using Math = System.Math;
 namespace Striver_DSA_A_Z._02_Arrays;
 
 public partial class Arrays{
-    
+
     public static void Rotate(int[] nums, int k)
     {
-        
-        if(nums.Length ==1)
-            return ;
-        var j = nums.Length - k;
-        if(j<0)
-        {
-           decimal val = j / nums.Length;
-            int totalIterations =(int) Math.Floor(val);
-            int totalSum = totalIterations * nums.Length;
-            j+=totalSum;
-        }
-        
+
+        int n =nums.Length;
+        if(k>n)
+            k = k %n;
+
+        int j = n-k;
         var endIndex = nums.Length-1;
         ReverseArray(nums,0,j-1);
         ReverseArray(nums,j,endIndex);
@@ -35,5 +29,5 @@ public partial class Arrays{
             endIndex--;
         }
     }
-    
+
 }
