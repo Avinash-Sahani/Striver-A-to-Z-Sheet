23a1798cb9fe4e33d44fa62190d39db44b215cb6
Avinash Sahani <avinashsahani66@gmail.com>
2024-06-22 16:53:21 -0400

    BinarySearch(1d) -> Basic Binary Search

---
+++ Striver-DSA-A-Z/03-BinarySearch/1D-Arrays/Binary-Search.cs
@@ -0,0 +1,26 @@
+﻿namespace Striver_DSA_A_Z._03_BinarySearch._1D_Arrays;
+
+public partial class BinarySearch
+{
+    public int BinarySearchNum(int [] nums,int low,int high,int target)
+    {
+
+        if(low>high)
+            return -1;
+
+        int mid = (low + high)/2;
+
+        if(nums[mid] == target)
+            return mid;
+        else if(nums[mid]<target)
+            return BinarySearchNum(nums,mid+1,high,target);
+        else
+            return BinarySearchNum(nums,low,mid-1,target);
+    }
+
+    public int Search(int[] nums, int target) {
+
+        return BinarySearchNum(nums,0,nums.Length-1,target);
+    }
+
+}
