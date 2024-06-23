c0429218517ca6e91dd1dbaa7621ca0fd35c964a
Avinash Sahani <avinashsahani66@gmail.com>
2024-06-23 18:05:27 -0400

    BinarySearch(1d) -> Floor Ceil 2nd implementaion

---
+++ Striver-DSA-A-Z/03-BinarySearch/1D-Arrays/04-Floor-Ceil.cs
@@ -0,0 +1,50 @@
+﻿namespace Striver_DSA_A_Z._03_BinarySearch;
+
+public partial class BinarySearch {
+
+    static int floor = -1;
+    static int ceil = -1;
+    public static int[] BsfloorCeil(int[]arr,int low,int high,int x)
+    {
+
+
+        if(low>high)
+        {
+            if(floor==-1)
+                ceil=arr[0];
+            return new int[]{floor, ceil};
+
+        }
+
+
+        int mid = (low + high) /2;
+        if(x == arr[mid])
+            return new int[] {arr[mid],arr[mid]};
+
+        if(arr[mid]<=x)
+        {
+            if(arr[mid]>floor)
+            {
+                floor= arr[mid];
+                if((mid+1) <arr.Length)
+                    if(arr[mid+1]>=x)
+                        ceil = arr[mid+1];
+
+            }
+            return BsfloorCeil(arr,mid+1,high,x);
+        }
+        else
+        {
+
+            return BsfloorCeil(arr,low,mid-1,x);
+        }
+
+
+
+    }
+    public static int[] getFloorAndCeil(int[] a, int n, int x) {
+
+        return BsfloorCeil(a,0,n-1,x);
+        // Wriute your code here.
+    }
+}
