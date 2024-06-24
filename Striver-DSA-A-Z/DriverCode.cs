8f358362b3f5f8a7aff45f4ad6e2f73dc00188c7
Avinash Sahani <avinashsahani66@gmail.com>
2024-06-24 10:16:08 -0400

    BS(1D-Array) : first occurence problem and count occurence

--- Striver-DSA-A-Z/DriverCode.cs
+++ Striver-DSA-A-Z/DriverCode.cs
@@ -1,16 +1,13 @@
-using Striver_DSA_A_Z._02_Arrays;
+ using Striver_DSA_A_Z._02_Arrays;
 using Striver_DSA_A_Z._03_BinarySearch;
 
 
 var bs = new BinarySearch();
-long[] array = new long[]
+var array = new int[]
 {
-    66, 67, 68, 69, 70, 71, 72, 73, 74, 75,
-    76, 77, 78, 79, 80, 81, 82, 83, 84, 85,
-    86, 87, 88, 89, 90, 91, 92, 93, 94, 95,
-    96, 97, 98, 99, 100, 101, 102, 103, 104, 105,
-    106, 107, 108, 109, 110, 111, 112, 113, 114, 115,
-    116, 117, 118, 119, 120, 121, 122, 123, 124, 125,
-    126, 127, 128, 129, 130
+    5,7,7,8,8,10
 };
-Console.WriteLine(bs.findFloor(array,65,106));
+foreach (var i in bs.SearchRange(array,8))
+{
+    Console.WriteLine(i + " ");
+}
