5f040361beebb1b286997a3ecda6e9bd40ff56e1
Avinash Sahani <avinashsahani66@gmail.com>
2024-06-24 15:51:26 -0400

    BS(1D-Array) : Rotated and sorted using BS

---
+++ Striver-DSA-A-Z/03-BinarySearch/1D-Arrays/07-Rotated-Sorted.cs
@@ -0,0 +1,35 @@
+﻿namespace Striver_DSA_A_Z._03_BinarySearch;
+
+public partial class  BinarySearch
+{
+    public int SearchInRotatedSorted(int[] nums, int target) {
+
+        int low  = 0;
+        int high = nums.Length -1;
+        while(low<=high)
+        {
+            int mid = low + (high-low)/2;
+            if(nums[mid]==target)
+                return mid;
+            else if(nums[low]<=nums[mid] )
+            {
+                if(target >=nums[low] && target<=nums[mid])
+                    high = mid -1 ;
+                else
+                    low =  mid +1;
+            }
+            else
+            {
+                if(target >=nums[mid] && target<=nums[high])
+                    low =  mid +1;
+                else
+                    high = mid -1;
+            }
+
+
+
+        }
+        return -1;
+    }
+}
+
--- Striver-DSA-A-Z/DriverCode.cs
+++ Striver-DSA-A-Z/DriverCode.cs
@@ -5,9 +5,5 @@ using Striver_DSA_A_Z._03_BinarySearch;
 var bs = new BinarySearch();
 var array = new int[]
 {
-    5,7,7,8,8,10
+    4,5,6,7,0,1,2
 };
-foreach (var i in bs.SearchRange(array,8))
-{
-    Console.WriteLine(i + " ");
-}
