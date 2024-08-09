de2494824245b1871d650c33213595a69cc5884c
Avinash Sahani <avinashsahani66@gmail.com>
2024-08-08 21:25:12 -0400

    BS(1D-Array) : add rotate image

---
+++ Striver-DSA-A-Z/02-Arrays/02-Medium/14-RotateImage.cs
@@ -0,0 +1,29 @@
+﻿namespace Striver_DSA_A_Z._02_Arrays;
+
+public partial class Arrays {
+    public void Rotate(int[][] matrix) {
+        int n = matrix.Length;
+        int m = matrix[0].Length;
+
+        for(int i=0; i<n; i++)
+        {
+            for(int j=i+1; j<m; j++)
+            {
+                (matrix[i][j],matrix[j][i]) = (matrix[j][i],matrix[i][j]);
+            }
+        }
+        for(int i=0;i<n;i++)
+        {
+            int k=m-1;
+            int j=0;
+            while(j<=k)
+            {
+                (matrix[i][j],matrix[i][k]) = (matrix[i][k],matrix[i][j]);
+                j++;
+                k--;
+            }
+
+        }
+
+    }
+}
