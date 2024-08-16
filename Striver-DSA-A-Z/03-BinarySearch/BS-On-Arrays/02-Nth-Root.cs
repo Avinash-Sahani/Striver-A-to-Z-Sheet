52ea46ceac87bdb085e509a756a681915ed3fb1e
Avinash Sahani <avinashsahani66@gmail.com>
2024-08-15 21:14:14 -0400

    BS(answers) : BST on answers

---
+++ Striver-DSA-A-Z/03-BinarySearch/BS-On-Arrays/02-Nth-Root.cs
@@ -0,0 +1,48 @@
+﻿namespace Striver_DSA_A_Z._03_BinarySearch.BS_On_Arrays;
+
+public partial class BinarySearch {
+    public int NthRoot(int n, int m)
+    {
+
+
+        if(m==1 || n==1)
+            return m;
+
+        int low = 1;
+        int high = m -1;
+        int ans = -1;
+        while(low<=high)
+        {
+
+            int mid = low + (high-low)/2;
+
+            long value = 1;
+            for(int i=1; i<=n; i++)
+            {
+                value*=mid;
+
+                if(value > m)
+                {
+                    high = mid-1;
+                    break;
+
+                }
+
+
+            }
+
+            if(value==m)
+            {
+
+                ans = mid;
+                break;
+            }
+            else if(value<m)
+                low = mid+1;
+
+
+
+        }
+        return ans;
+    }
+}
---
+++ Striver-DSA-A-Z/03-BinarySearch/BS-On-Arrays/03-Kth-Positive-Number.cs
@@ -0,0 +1,22 @@
+﻿namespace Striver_DSA_A_Z._03_BinarySearch.BS_On_Arrays;
+
+public partial class BinarySearch {
+    public int FindKthPositive(int[] arr, int k) {
+
+        int low = 0;
+        int high = arr.Length-1;
+        int mid = -1;
+        int missing = 0;
+        while(low<=high)
+        {
+            mid = (low+high)/2;
+            missing  = arr[mid]-(mid+1);
+            if(missing<k)
+                low = mid+1;
+            else
+                high = mid-1;
+
+        }
+        return k+high+1;
+    }
+}
