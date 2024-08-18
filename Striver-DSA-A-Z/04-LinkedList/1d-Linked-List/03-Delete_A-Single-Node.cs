12526deb8283958edcda8a98da2fb787fc451828
Avinash Sahani <avinashsahani66@gmail.com>
2024-08-18 02:38:11 -0400

    BS(answers) : easy problem of singly LL

---
+++ Striver-DSA-A-Z/04-LinkedList/1d-Linked-List/03-Delete_A-Single-Node.cs
@@ -0,0 +1,15 @@
+﻿namespace Striver_DSA_A_Z._04_LinkedList._1d_Linked_List;
+
+public partial class LinkedList {
+    public void DeleteNode(ListNode node) {
+        node.val = node.next.val;
+        node.next = node.next.next;
+    }
+}
+
+
+  public class ListNode {
+      public int val;
+      public ListNode next;
+      public ListNode(int x) { val = x; }
+  }
