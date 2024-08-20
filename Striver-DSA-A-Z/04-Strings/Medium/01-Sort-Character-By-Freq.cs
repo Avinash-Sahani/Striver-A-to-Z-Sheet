ce62facb6b767106cb937ae186254a4a8fd56b3a
Avinash Sahani <avinashsahani66@gmail.com>
2024-08-20 01:55:45 -0400

    Strings(Medium): Add strings medium problems

---
+++ Striver-DSA-A-Z/04-Strings/Medium/01-Sort-Character-By-Freq.cs
@@ -0,0 +1,26 @@
+﻿namespace Striver_DSA_A_Z._04_Strings.Medium;
+
+public partial class Strings {
+    public string FrequencySort(string s) {
+
+        Dictionary<char,int> freq = new Dictionary<char,int>();
+        for(int i=0; i<s.Length;i++)
+        {
+            if(freq.ContainsKey(s[i]))
+                freq[s[i]]+=1;
+            else
+                freq[s[i]]=1;
+
+        }
+        var sortedDictionary = freq.OrderByDescending(item =>item.Value);
+
+        string ans ="";
+        foreach(var (key,value) in sortedDictionary)
+        {
+
+            for(int i=0; i<value; i++)
+                ans+=key;
+        }
+        return ans;
+    }
+}
