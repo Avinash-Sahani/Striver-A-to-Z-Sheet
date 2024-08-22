74cf4dc06688bb2168649a8c5d7af683a32e35b0
Avinash Sahani <avinashsahani66@gmail.com>
2024-08-22 12:30:28 -0400

    Recursion : Power-Base-Exponent

---
+++ Striver-DSA-A-Z/06-Recursion/Pow-Base-Exponenet.cs
@@ -0,0 +1,38 @@
+﻿namespace Striver_DSA_A_Z._06_Recursion;
+//Problem Link : https://leetcode.com/problems/powx-n/
+public partial class Recursion
+{
+
+    public double MyPow(double x, int n) {
+        if(n==1)
+            return x;
+
+        double ans = 1;
+        var isNegative  = false;
+        if(n<0)
+        {
+            isNegative = true;
+            n = n*-1;
+        }
+        while(n>0)
+        {
+            if(n%2==0)
+            {
+                x = x *x;
+                n/=2;
+            }
+            else
+            {
+
+                n = n -1;
+                ans = ans *x;
+            }
+
+
+        }
+
+        return isNegative ?(double)(1.0) / (double)(ans) :ans;
+
+
+    }
+}
